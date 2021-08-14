70c514dc6a3ac16e8e75405f9f9f87824144dd84
parth chugh <parthchugh52@gmail.com>
2021-08-14 19:39:10 +0530

    fixed the flow for products

--- src/ShowProducts.js
+++ src/ShowProducts.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+import styles from "./styles";
 
-export default function ShowProducts({products}) {
-    return (
-        <div>
-            
-        </div>
-    )
+export default function ShowProducts({ products }) {
+  return (
+    <div className="form-web-scrape">
+      {products.map((el) => (
+        <span style={styles.colorWhite}>{el.name} </span>
+      ))}
+    </div>
+  );
 }
--- src/styles.js
+++ src/styles.js
@@ -1,24 +1,24 @@
 export default {
-    heading: {
-        color: 'white', 
-        fontSize: 30, 
-        width: "40%", 
-        textAlign: 'center',
-        marginBottom: 10
-    },
-    errorMessage: {
-        color: 'orange',
-        marginTop: 10,
-        marginBottom: 10
-    },
-    colorWhite: {
-        color: 'white'
-    },
-    marginBottom: {
-        marginBottom: 10
-    },
-    label: {
-        color: 'white',
-        marginBottom: 5
-    }
-}
+  heading: {
+    color: "white",
+    fontSize: 30,
+    width: "40%",
+    textAlign: "center",
+    marginBottom: 10,
+  },
+  errorMessage: {
+    color: "orange",
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  colorWhite: {
+    color: "white",
+  },
+  marginBottom: {
+    marginBottom: 10,
+  },
+  label: {
+    color: "white",
+    marginBottom: 5,
+  },
+};
